edd5a739da28bcf0dca343b55e9c0732764c91f7
Yonatan Attali <yonatan.attali@gmail.com>
2021-06-12 12:17:56 -0400

    added iceberg sprites and random icebergs in stage scene

--- src/Scripts/PlatformSpawner.gd
+++ src/Scripts/PlatformSpawner.gd
@@ -3,12 +3,22 @@ extends Path2D
 export var speed = 400
 onready var platform_tscn = preload("res://Scenes/platform.tscn")
 
+onready var platform_tscns = [
+	preload("res://Scenes/Icebergs/iceberg-1.tscn"),
+	preload("res://Scenes/Icebergs/iceberg-2.tscn"),
+	preload("res://Scenes/Icebergs/iceberg-2b.tscn"),
+	preload("res://Scenes/Icebergs/iceberg-3.tscn"),
+	preload("res://Scenes/Icebergs/iceberg-4.tscn")
+]
+
 func _physics_process(delta):
 	$PathFollow2D.offset += speed * delta
 
 
 func _on_SpawnTimer_timeout():
-	var platform = platform_tscn.instance()
+	var platform_scene = platform_tscns[randi() % platform_tscns.size()]
+	var platform = platform_scene.instance()
+#	var platform = platform_tscn.instance()
 	platform.position = $PathFollow2D.position
 	$SpawnedPlatforms.add_child(platform)
 	print("spawning")
