dee5da658eaa1bf1a78ef40179d337b6dec98952
Yonatan Attali <yonatan.attali@gmail.com>
2021-06-12 18:43:13 -0400

    started working on credits scene
    packed the line spawner and the waterfall spawner into scenes

--- src/Scripts/GUI/GUI.gd
+++ src/Scripts/GUI/GUI.gd
@@ -15,3 +15,15 @@ func _on_Options_pressed():
 func _on_Back_pressed():
 	$TitleScreen.show()
 	$ControlsScreen.hide()
+
+
+func _on_CreditsScreen_gui_input(event):
+	event = event as InputEventMouseButton
+	if event:
+		pass
+	pass # Replace with function body.
+
+
+func transition():
+	# fades to black then back to white
+	pass
