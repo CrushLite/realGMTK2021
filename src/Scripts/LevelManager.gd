773df3eee94dbfec251b9ac34f3d54a36c7badf7
Yonatan Attali <yonatan.attali@gmail.com>
2021-06-12 01:44:31 -0400

    Created levelmanager  and main menu screen

--- src/Scripts/LevelManager.gd
+++ src/Scripts/LevelManager.gd
@@ -1,16 +1,12 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+onready var main_scene = preload("res://Scenes/Stage.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$GUI.connect("play_pressed", self, "_on_play_pressed")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_play_pressed():
+	var scene = main_scene.instance()
+	add_child(scene)
